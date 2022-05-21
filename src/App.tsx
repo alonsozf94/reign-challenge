26c2c216977d5d4f6a49e45f72580171c5c1b379
alonsozf94 <alonsozf.94@gmail.com>
2022-05-20 20:22:23 -0500

    Initial push

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { BiTime } from 'react-icons/bi';
+import moment from 'moment';
 import './App.css';
 
 function App() {
+
+  const [news, setNews] = useState<Array<any>>([])
+
+  async function fetchNews(library: string, page: number) {
+    const response = await fetch(`https://hn.algolia.com/api/v1/search_by_date?query=${library}&page=${page}`)
+    const data = await response.json();
+    return data.hits
+  }
+
+  useEffect(() => {
+    const getNews = async () => {
+      //const newsFromAPI = await fetchNews('angular');
+      setNews(await fetchNews('angular',0));
+    }
+    getNews()
+    console.log(news)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        Hello World
       </header>
+      <ul className="article-list">
+        {
+          news.map(element => (
+          <li className="article">
+            <div className="article-content">
+              <div className="article-info">
+                <BiTime />{moment(element.created_at).fromNow()} by {element.author}
+              </div>
+              <div className="article-desc">{element.story_title}</div>
+            </div>
+            <div className="article-favorite">
+              F
+            </div>
+
+          </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
